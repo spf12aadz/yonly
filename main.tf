2282818bf143c6e13c88fbb5c1041b83738bf6da
spf12aadz <47715967+spf12aadz@users.noreply.github.com>
2023-09-05 14:39:57 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-051f7e7f6c2f40dc1" # us-west-2
   instance_type = "t2.micro"  
 }
